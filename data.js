3ff1dcbd973b0809186d649cee978bc6fd599222
Joshhortt <joshhortt@yahoo.com>
2022-12-17 10:56:09 +0000

    add styles with google fonts

---
+++ data.js
@@ -0,0 +1,10 @@
+const CATEGORIAS = [
+  { name: "Iniciação à Costura", color: "#3b82f6" },
+  { name: "Projeto de Moda Criativo", color: "#16a34a" },
+  { name: "Corte de Saias e Vestidos", color: "#ef4444" },
+  { name: "Corte de Calças", color: "#eab308" },
+  { name: "Corte de Camisas", color: "#db2777" },
+  { name: "Costura Criativa", color: "#14b8a6" },
+  { name: "Técnicas de Costura", color: "#f97316" },
+  { name: "Conteúdos do Formador", color: "#8b5cf6" },
+];
