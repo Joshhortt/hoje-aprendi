76dad78a5d1fbcd833c29a3ba2d8bbdbbb8a4e9d
Joshhortt <joshhortt@yahoo.com>
2022-12-23 13:54:16 +0000

    add to html: btn class

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Ola Hoje Aprendi!");
+
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Fechar";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Partilhar";
+  }
+});
