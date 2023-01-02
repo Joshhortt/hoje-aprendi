1a5835c80a51147aa8ddc60d33b9767bbf0b3d60
Joshhortt <joshhortt@yahoo.com>
2023-01-02 17:43:57 +0000

    add:(App.js) divide Inteface into more Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import "./style.css";
 
 function App() {
+  const appTitle = "Hoje Aprendi!";
   return (
     <>
       {/*HEADER*/}
       <header className="header">
         <div className="logo">
           <img src="logo.png" height="68" width="68" alt="Hoje Aprendi!" />
-          <h1>Hoje Aprendi!</h1>
+          <h1>{appTitle}</h1>
         </div>
         <button className="btn btn-large btn-open">Partilhar</button>
       </header>
-
-      <CategoryFilter />
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
     </>
   );
 }
-// Second Component
+
+// New Fact Form Component
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+// Category Filter Component
 function CategoryFilter() {
   return <aside>Category filter</aside>;
+}
+
+// Fact List Component
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
