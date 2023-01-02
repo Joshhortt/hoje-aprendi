793905e9516bc388f3326e962f7ae0900f963612
Joshhortt <joshhortt@yahoo.com>
2023-01-02 14:19:59 +0000

    Initialize Create App Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import "./style.css";
+
 function App() {
-  return <h1>hoje-aprendi!</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Hoje Aprendi!" />
+        <h1>Hoje Aprendi!</h1>
+      </div>
+      <button className="btn btn-large btn-open">Partilhar</button>
+    </header>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,3 +8,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+export default App;
