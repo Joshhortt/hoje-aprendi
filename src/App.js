2b938f459ae20d26c54d3d44a11ca88c2172b433
Joshhortt <joshhortt@yahoo.com>
2023-01-02 19:16:59 +0000

    add (App.sj): rendering the List of Facts.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
 import "./style.css";
 
+// Data take from v1/data.js
+const CATEGORIES = [
+  {
+    name: "Ver Tudo",
+    color: "linear-gradient(135deg, #3b82f6, #ef4444, #16a34a, #eab308",
+  },
+  { name: "Iniciação à Costura", color: "#3b82f6" },
+  { name: "Projeto de Moda Criativo", color: "#16a34a" },
+  { name: "Corte de Saias e Vestidos", color: "#ef4444" },
+  { name: "Corte de Calças", color: "#eab308" },
+  { name: "Corte de Camisas", color: "#db2777" },
+  { name: "Costura Criativa", color: "#14b8a6" },
+  { name: "Técnicas de Costura", color: "#f97316" },
+  { name: "Conteúdos do Formador", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "Como começar a costurar: 1o dicas para iniciantes",
+    source: "https://alegro.pt/lets-talk/como-comecar-costurar",
+    category: "intro-costura",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2022,
+  },
+  {
+    id: 2,
+    text: "Processo criativo no design de moda: como explorar o seu?",
+    source:
+      "https://www.digitaletextil.com.br/blog/processo-criativo-design-de-moda/",
+    category: "projeto-moda-criativo",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2022,
+  },
+  {
+    id: 3,
+    text: "Livro Corte e Costura. O Guia Definitivo de Técnicas Para a Produção de Blazers, Blusas, Calças, Saias e Vestidos",
+    source:
+      "https://indicalivros.com/livros/corte-e-costura-o-guia-definitivo-de-tecnicas-para-a-producao-de-blazers-blusas-calcas-saias-e-vestidos-alison-smith",
+    category: "corte-saias-vestidos",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2022,
+  },
+];
+
 function App() {
   const appTitle = "Hoje Aprendi!";
   return (
@@ -33,7 +84,39 @@ function CategoryFilter() {
 
 // Fact List Component
 function FactList() {
-  return <section>Facts list</section>;
+  // Temporary
+  const facts = initialFacts;
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <li key={fact.id} className="fact">
+            <p>
+              {fact.text}
+              <a className="source" href={fact.source} target="blank">
+                (Link)
+              </a>
+            </p>
+            <span
+              className="tag"
+              style={{
+                backgroundColor: CATEGORIES.find(
+                  (cat) => cat.name === fact.category
+                )?.color,
+              }}
+            >
+              {fact.category}
+            </span>
+            <div className="vote-buttons">
+              <button>👍{fact.votesInteresting}</button>
+              <button>🤯 {fact.votesMindblowing}</button>
+              <button>⛔️ {fact.votesFalse}</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default App;
--- v1/data.js
+++ v1/data.js
@@ -1,4 +1,4 @@
-const CATEGORIAS = [
+const CATEGORIES = [
   {
     name: "Ver Tudo",
     color: "linear-gradient(135deg, #3b82f6, #ef4444, #16a34a, #eab308",
@@ -11,4 +11,39 @@ const CATEGORIAS = [
   { name: "Costura Criativa", color: "#14b8a6" },
   { name: "Técnicas de Costura", color: "#f97316" },
   { name: "Conteúdos do Formador", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "Como começar a costurar: 1o dicas para iniciantes",
+    source: "https://alegro.pt/lets-talk/como-comecar-costurar",
+    category: "intro-costura",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2022,
+  },
+  {
+    id: 2,
+    text: "Processo criativo no design de moda: como explorar o seu?",
+    source:
+      "https://www.digitaletextil.com.br/blog/processo-criativo-design-de-moda/",
+    category: "projeto-moda-criativo",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2022,
+  },
+  {
+    id: 3,
+    text: "Livro Corte e Costura. O Guia Definitivo de Técnicas Para a Produção de Blazers, Blusas, Calças, Saias e Vestidos",
+    source:
+      "https://indicalivros.com/livros/corte-e-costura-o-guia-definitivo-de-tecnicas-para-a-producao-de-blazers-blusas-calcas-saias-e-vestidos-alison-smith",
+    category: "corte-saias-vestidos",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2022,
+  },
 ];
