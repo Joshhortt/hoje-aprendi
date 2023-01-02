5f905bf1327851372e1e9a75a5633f0ef4146b0d
Joshhortt <joshhortt@yahoo.com>
2023-01-02 14:31:12 +0000

    add CategoryFilter component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import "./style.css";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Hoje Aprendi!" />
-        <h1>Hoje Aprendi!</h1>
-      </div>
-      <button className="btn btn-large btn-open">Partilhar</button>
-    </header>
+    <>
+      {/*HEADER*/}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" height="68" width="68" alt="Hoje Aprendi!" />
+          <h1>Hoje Aprendi!</h1>
+        </div>
+        <button className="btn btn-large btn-open">Partilhar</button>
+      </header>
+
+      <CategoryFilter />
+    </>
   );
+}
+// Second Component
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
 }
 
 export default App;
