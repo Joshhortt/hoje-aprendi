b1db30e5318ea2ee9c2887e97c82296610d390b5
Joshhortt <joshhortt@yahoo.com>
2023-01-05 10:44:18 +0000

    refactor: list item html to jsx, delete comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
 import "./style.css";
 
-// Data take from v1/data.js
-const CATEGORIES = [
-  {
-    name: "Ver Tudo",
-    color: "linear-gradient(135deg, #3b82f6, #ef4444, #16a34a, #eab308",
-  },
-  { name: "Iniciação à Costura", color: "#3b82f6" },
-  { name: "Projeto de Moda Criativo", color: "#16a34a" },
-  { name: "Corte de Saias e Vestidos", color: "#ef4444" },
-  { name: "Corte de Calças", color: "#eab308" },
-  { name: "Corte de Camisas", color: "#db2777" },
-  { name: "Costura Criativa", color: "#14b8a6" },
-  { name: "Técnicas de Costura", color: "#f97316" },
-  { name: "Conteúdos do Formador", color: "#8b5cf6" },
-];
-
 const initialFacts = [
   {
     id: 1,
@@ -77,9 +61,38 @@ function NewFactForm() {
   return <form className="fact-form">Fact form</form>;
 }
 
+// Data take from v1/data.js
+const CATEGORIES = [
+  {
+    name: "Ver Tudo",
+    color: "linear-gradient(135deg, #3b82f6, #ef4444, #16a34a, #eab308",
+  },
+  { name: "Iniciação à Costura", color: "#3b82f6" },
+  { name: "Projeto de Moda Criativo", color: "#16a34a" },
+  { name: "Corte de Saias e Vestidos", color: "#ef4444" },
+  { name: "Corte de Calças", color: "#eab308" },
+  { name: "Corte de Camisas", color: "#db2777" },
+  { name: "Costura Criativa", color: "#14b8a6" },
+  { name: "Técnicas de Costura", color: "#f97316" },
+  { name: "Conteúdos do Formador", color: "#8b5cf6" },
+];
+
 // Category Filter Component
 function CategoryFilter() {
-  return <aside>Category filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button
+            className="btn btn-category"
+            style={{ backgroundColor: "#3b82f6" }}
+          >
+            Iniciação à Costura
+          </button>
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
 // Fact List Component
