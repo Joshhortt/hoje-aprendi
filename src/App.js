79b6b968440aebd329f2837e8a1843c1ff7d0f68
Joshhortt <joshhortt@yahoo.com>
2023-01-12 12:05:12 +0000

    refactor: update categories names; add initialFact

--- src/App.js
+++ src/App.js
@@ -6,33 +6,44 @@ const initialFacts = [
     id: 1,
     text: "Como começar a costurar: 1o dicas para iniciantes",
     source: "https://alegro.pt/lets-talk/como-comecar-costurar",
-    category: "introdução à costura",
+    category: "transformação moldes de vestidos",
     votesInteresting: 24,
     votesMindblowing: 9,
     votesFalse: 4,
-    createdIn: 2022,
+    createdIn: 2023,
   },
   {
     id: 2,
     text: "Processo criativo no design de moda: como explorar o seu?",
     source:
       "https://www.digitaletextil.com.br/blog/processo-criativo-design-de-moda/",
-    category: "projeto de moda criativo",
+    category: "modelação protótipos",
     votesInteresting: 11,
     votesMindblowing: 2,
     votesFalse: 0,
-    createdIn: 2022,
+    createdIn: 2023,
   },
   {
     id: 3,
     text: "Livro Corte e Costura. O Guia Definitivo de Técnicas Para a Produção de Blazers, Blusas, Calças, Saias e Vestidos",
     source:
       "https://indicalivros.com/livros/corte-e-costura-o-guia-definitivo-de-tecnicas-para-a-producao-de-blazers-blusas-calcas-saias-e-vestidos-alison-smith",
-    category: "corte de saias e vestidos",
+    category: "confeção protótipos",
     votesInteresting: 8,
     votesMindblowing: 3,
     votesFalse: 1,
-    createdIn: 2022,
+    createdIn: 2023,
+  },
+  {
+    id: 4,
+    text: "Livro Corte e Costura. O Guia Definitivo de Técnicas Para a Produção de Jeans",
+    source:
+      "https://indicalivros.com/livros/corte-e-costura-o-guia-definitivo-de-tecnicas-para-a-producao-de-blazers-blusas-calcas-saias-e-vestidos-alison-smith",
+    category: "confeção peças finais",
+    votesInteresting: 7,
+    votesMindblowing: 2,
+    votesFalse: 3,
+    createdIn: 2023,
   },
 ];
 
@@ -78,15 +89,20 @@ function Header({ showForm, setShowForm }) {
 
 // Data take from v1/data.js
 const CATEGORIES = [
-  { name: "Iniciação à Costura", color: "#3b82f6" },
-  { name: "Projeto de Moda Criativo", color: "#16a34a" },
-  { name: "Corte de Saias e Vestidos", color: "#ef4444" },
-  { name: "Corte de Calças", color: "#eab308" },
-  { name: "Corte de Camisas", color: "#db2777" },
-  { name: "Costura Criativa", color: "#14b8a6" },
-  { name: "Técnicas de Costura", color: "#f97316" },
+  { name: "Trans. Moldes de vestidos", color: "#3b82f6" },
+  { name: "Modelação de protótipos", color: "#16a34a" },
+  { name: "Confeção de protótipos", color: "#ef4444" },
+  { name: "Confeção de peças finais", color: "#eab308" },
+  { name: "D.P. e Téc. procura emprego", color: "#db2777" },
+  { name: "Informações da Inovinter", color: "#f97316" },
   { name: "Conteúdos do Formador", color: "#8b5cf6" },
 ];
+
+//1975 - Transformação de moldes de vestidos - 25
+//1990 - Modelação de protótipos - 50
+//1991 - Confeção de protótipos -50
+//1992 - Confeção de peças finais - 50
+//8598 - Desenvolvimento pessoal e técnicas de procura de emprego
 
 function isValidHttpUrl(string) {
   let url;
